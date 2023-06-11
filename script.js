6d6a5b3ec89e059af59baac35213a1ad7837e5cc
netphoria <96025941+netphoria@users.noreply.github.com>
2023-06-11 03:39:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var cartItems = []; // Array to store cart items
 // Function to add an item to the cart
 function addItemToCart() {
   var item = {
-    name: "Product Name",
+    name: "Trial 01",
     quantity: 1,
     unit_amount: {
       currency_code: "USD",
@@ -17,6 +17,9 @@ function addItemToCart() {
 
   // Render cart summary
   renderCartSummary();
+
+  // Render PayPal cart buttons
+  renderPayPalButtons();
 }
 
 // Function to render the cart summary
@@ -35,6 +38,42 @@ function renderCartSummary() {
   cartSummaryHTML += "<p>Total Amount: $" + totalAmount.toFixed(2) + "</p>";
 
   document.getElementById("cartSummary").innerHTML = cartSummaryHTML;
+}
+
+// Function to render PayPal cart buttons
+function renderPayPalButtons() {
+  paypal.Buttons({
+    createOrder: function(data, actions) {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: getTotalCartAmount()
+            },
+            items: cartItems
+          }
+        ]
+      });
+    },
+    onApprove: function(data, actions) {
+      return actions.order.capture().then(function(details) {
+        // Payment successful, unlock the content by removing the add to cart button
+        document.getElementById("addCartButton").remove();
+        document.getElementById("paypalCartContainer").innerHTML =
+          "<p>Payment successful!</p>";
+      });
+    }
+  }).render("#paypalCartContainer");
+}
+
+// Function to calculate the total cart amount
+function getTotalCartAmount() {
+  var total = 0;
+  for (var i = 0; i < cartItems.length; i++) {
+    total += cartItems[i].unit_amount.value * cartItems[i].quantity;
+  }
+  return total.toFixed(2);
 }
 
 // Get the add to cart button element
