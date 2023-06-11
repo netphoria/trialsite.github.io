93f05cceae80b209f44b5fc00385dea7480a710d
netphoria <96025941+netphoria@users.noreply.github.com>
2023-06-11 03:35:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,44 +15,26 @@ function addItemToCart() {
   // Add item to cart
   cartItems.push(item);
 
-  // Render PayPal cart buttons
-  renderPayPalButtons();
+  // Render cart summary
+  renderCartSummary();
 }
 
-// Function to render PayPal cart buttons
-function renderPayPalButtons() {
-  paypal.Buttons({
-    createOrder: function(data, actions) {
-      return actions.order.create({
-        purchase_units: [
-          {
-            amount: {
-              currency_code: "USD",
-              value: getTotalCartAmount()
-            },
-            items: cartItems
-          }
-        ]
-      });
-    },
-    onApprove: function(data, actions) {
-      return actions.order.capture().then(function(details) {
-        // Payment successful, unlock the content by removing the add to cart button
-        document.getElementById("addCartButton").remove();
-        document.getElementById("paypalCartContainer").innerHTML =
-          "<p>Payment successful!</p>";
-      });
-    }
-  }).render("#paypalCartContainer");
-}
+// Function to render the cart summary
+function renderCartSummary() {
+  var cartSummaryHTML = "<h3>Cart Summary</h3>";
+  var totalAmount = 0;
 
-// Function to calculate the total cart amount
-function getTotalCartAmount() {
-  var total = 0;
   for (var i = 0; i < cartItems.length; i++) {
-    total += cartItems[i].unit_amount.value * cartItems[i].quantity;
+    var item = cartItems[i];
+    var itemTotal = item.quantity * item.unit_amount.value;
+    totalAmount += itemTotal;
+
+    cartSummaryHTML += "<p>" + item.name + " - Quantity: " + item.quantity + " - Total: $" + itemTotal.toFixed(2) + "</p>";
   }
-  return total.toFixed(2);
+
+  cartSummaryHTML += "<p>Total Amount: $" + totalAmount.toFixed(2) + "</p>";
+
+  document.getElementById("cartSummary").innerHTML = cartSummaryHTML;
 }
 
 // Get the add to cart button element
